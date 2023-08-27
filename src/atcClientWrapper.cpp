af162166a5ad169d83023abd7f5ded37d2a291dc
Pierre Ferran <pierr3@users.noreply.github.com>
2023-08-27 14:44:32 +0200

    Reset radio stack on disconnect

--- src/atcClientWrapper.cpp
+++ src/atcClientWrapper.cpp
@@ -1,328 +1,337 @@
-#include <string>
-#include <thread>
+#include <algorithm>
 #include <atomic>
 #include <functional>
+#include <iterator>
 #include <mutex>
-#include <algorithm>
-#include <iterator> 
+#include <string>
+#include <thread>
 
 #include "afv-native/Log.h"
-#include "afv-native/atcClientWrapper.h"
 #include "afv-native/atcClient.h"
+#include "afv-native/atcClientWrapper.h"
 
 #include <event2/event.h>
 
-namespace atcapi
-{
-    struct event_base* ev_base;
+namespace atcapi {
+struct event_base *ev_base;
 
-    std::mutex afvMutex;
-    std::unique_ptr<afv_native::ATCClient> client;
-    std::unique_ptr<std::thread> eventThread;
-    std::atomic<bool>keepAlive{ false };
-    std::atomic<bool> isInitialized{ false };
-}
+std::mutex afvMutex;
+std::unique_ptr<afv_native::ATCClient> client;
+std::unique_ptr<std::thread> eventThread;
+std::atomic<bool> keepAlive{false};
+std::atomic<bool> isInitialized{false};
+} // namespace atcapi
 
 using namespace atcapi;
 
 void afv_native::api::atcClient::setLogger(afv_native::log_fn gLogger) {
-    afv_native::setLogger(gLogger);
+  afv_native::setLogger(gLogger);
 }
 
-afv_native::api::atcClient::atcClient(std::string clientName, std::string resourcePath) {
-        #ifdef WIN32
-            WORD wVersionRequested;
-            WSADATA wsaData;
-            wVersionRequested = MAKEWORD(2, 2);
-            WSAStartup(wVersionRequested, &wsaData);
-        #endif
+afv_native::api::atcClient::atcClient(std::string clientName,
+                                      std::string resourcePath) {
+#ifdef WIN32
+  WORD wVersionRequested;
+  WSADATA wsaData;
+  wVersionRequested = MAKEWORD(2, 2);
+  WSAStartup(wVersionRequested, &wsaData);
+#endif
 
-        ev_base = event_base_new();
+  ev_base = event_base_new();
 
-        client = std::make_unique<afv_native::ATCClient>(ev_base, resourcePath, clientName);
+  client = std::make_unique<afv_native::ATCClient>(ev_base, resourcePath,
+                                                   clientName);
 
-        keepAlive = true;
-        eventThread = std::make_unique<std::thread>([]
-        {
-            while (keepAlive)
-            {
-                event_base_loop(ev_base, EVLOOP_NONBLOCK);
-                std::this_thread::sleep_for(std::chrono::milliseconds(10));
-            }
-        });
+  keepAlive = true;
+  eventThread = std::make_unique<std::thread>([] {
+    while (keepAlive) {
+      event_base_loop(ev_base, EVLOOP_NONBLOCK);
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+  });
 
-        isInitialized = true;
+  isInitialized = true;
 }
 
 afv_native::api::atcClient::~atcClient() {
-    keepAlive = false;
-    if (eventThread->joinable())
-    {
-        eventThread->join();
-    }
-    client.reset();
-    isInitialized = false;
-    #ifdef WIN32
-        WSACleanup();
-    #endif
+  keepAlive = false;
+  if (eventThread->joinable()) {
+    eventThread->join();
+  }
+  client.reset();
+  isInitialized = false;
+#ifdef WIN32
+  WSACleanup();
+#endif
 }
 
-bool afv_native::api::atcClient::IsInitialized() {
-    return isInitialized;
-}
+bool afv_native::api::atcClient::IsInitialized() { return isInitialized; }
 
-void afv_native::api::atcClient::SetCredentials(std::string username, std::string password) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setCredentials(std::string(username), std::string(password));
+void afv_native::api::atcClient::SetCredentials(std::string username,
+                                                std::string password) {
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setCredentials(std::string(username), std::string(password));
 }
 
 void afv_native::api::atcClient::SetCallsign(std::string callsign) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setCallsign(std::string(callsign));
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setCallsign(std::string(callsign));
 }
 
-void afv_native::api::atcClient::SetClientPosition(double lat, double lon, double amslm, double aglm) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setClientPosition(lat, lon, amslm, aglm);
+void afv_native::api::atcClient::SetClientPosition(double lat, double lon,
+                                                   double amslm, double aglm) {
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setClientPosition(lat, lon, amslm, aglm);
 }
 
 bool afv_native::api::atcClient::IsVoiceConnected() {
-    return client->isVoiceConnected();
+  return client->isVoiceConnected();
 }
 
 bool afv_native::api::atcClient::IsAPIConnected() {
-    return client->isAPIConnected();
+  return client->isAPIConnected();
 }
 
 bool afv_native::api::atcClient::Connect() {
-    return client->connect();
+  std::lock_guard<std::mutex> lock(afvMutex);
+  return client->connect();
 }
 
 void afv_native::api::atcClient::Disconnect() {
-    return client->disconnect();
+  std::lock_guard<std::mutex> lock(afvMutex);
+  return client->disconnect();
 }
 
 void afv_native::api::atcClient::SetAudioApi(unsigned int api) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setAudioApi(api);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setAudioApi(api);
 }
 
 std::map<unsigned int, std::string> afv_native::api::atcClient::GetAudioApis() {
-    return afv_native::audio::AudioDevice::getAPIs();
+  return afv_native::audio::AudioDevice::getAPIs();
 }
 
 void afv_native::api::atcClient::SetAudioInputDevice(std::string inputDevice) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setAudioInputDevice(inputDevice);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setAudioInputDevice(inputDevice);
 }
 
-void afv_native::api::atcClient::SetAudioOutputDevice(std::string outputDevice) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setAudioOutputDevice(outputDevice);
+void afv_native::api::atcClient::SetAudioOutputDevice(
+    std::string outputDevice) {
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setAudioOutputDevice(outputDevice);
 }
 
-void afv_native::api::atcClient::SetAudioSpeakersOutputDevice(std::string outputDevice) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setSpeakerOutputDevice(outputDevice);
+void afv_native::api::atcClient::SetAudioSpeakersOutputDevice(
+    std::string outputDevice) {
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setSpeakerOutputDevice(outputDevice);
 }
 
 void afv_native::api::atcClient::SetHeadsetOutputChannel(int channel) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setHeadsetOutputChannel(channel);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setHeadsetOutputChannel(channel);
 }
 
-std::vector<std::string> afv_native::api::atcClient::GetAudioInputDevices(unsigned int mAudioApi) {
-    std::vector<std::string> out;
-    auto devices = afv_native::audio::AudioDevice::getCompatibleInputDevicesForApi(mAudioApi);
-    
-    std::transform(devices.begin(), devices.end(), std::back_inserter(out),
-    [](auto &kv){ return kv.second.name;});
+std::vector<std::string>
+afv_native::api::atcClient::GetAudioInputDevices(unsigned int mAudioApi) {
+  std::vector<std::string> out;
+  auto devices =
+      afv_native::audio::AudioDevice::getCompatibleInputDevicesForApi(
+          mAudioApi);
 
-    return out;
+  std::transform(devices.begin(), devices.end(), std::back_inserter(out),
+                 [](auto &kv) { return kv.second.name; });
+
+  return out;
 }
 
-std::vector<std::string> afv_native::api::atcClient::GetAudioOutputDevices(unsigned int mAudioApi) {
-    std::vector<std::string> out;
-    auto devices = afv_native::audio::AudioDevice::getCompatibleOutputDevicesForApi(mAudioApi);
-    
-    std::transform(devices.begin(), devices.end(), std::back_inserter(out),
-    [](auto &kv){ return kv.second.name;});
+std::vector<std::string>
+afv_native::api::atcClient::GetAudioOutputDevices(unsigned int mAudioApi) {
+  std::vector<std::string> out;
+  auto devices =
+      afv_native::audio::AudioDevice::getCompatibleOutputDevicesForApi(
+          mAudioApi);
 
-    return out;
+  std::transform(devices.begin(), devices.end(), std::back_inserter(out),
+                 [](auto &kv) { return kv.second.name; });
+
+  return out;
 }
 
 double afv_native::api::atcClient::GetInputPeak() const {
-    return client->getInputPeak();
+  return client->getInputPeak();
 }
 
 double afv_native::api::atcClient::GetInputVu() const {
-    return client->getInputVu();
+  return client->getInputVu();
 }
 
-void afv_native::api::atcClient::SetEnableInputFilters(bool enableInputFilters) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setEnableInputFilters(enableInputFilters);
+void afv_native::api::atcClient::SetEnableInputFilters(
+    bool enableInputFilters) {
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setEnableInputFilters(enableInputFilters);
 }
 
 void afv_native::api::atcClient::SetEnableOutputEffects(bool enableEffects) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setEnableOutputEffects(enableEffects);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setEnableOutputEffects(enableEffects);
 }
 
 bool afv_native::api::atcClient::GetEnableInputFilters() const {
-    return client->getEnableInputFilters();
+  return client->getEnableInputFilters();
 }
 
-void afv_native::api::atcClient::StartAudio() {
-    client->startAudio();
-}
+void afv_native::api::atcClient::StartAudio() { client->startAudio(); }
 
-void afv_native::api::atcClient::StopAudio() {
-    client->stopAudio();
-}
+void afv_native::api::atcClient::StopAudio() { client->stopAudio(); }
 
 bool afv_native::api::atcClient::IsAudioRunning() {
-    if (!client->mAudioDevice)
-        return false;
-    else
-        return true;
+  if (!client->mAudioDevice)
+    return false;
+  else
+    return true;
 }
 
 void afv_native::api::atcClient::SetTx(unsigned int freq, bool active) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setTx(freq, active);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setTx(freq, active);
 }
 
 void afv_native::api::atcClient::SetRx(unsigned int freq, bool active) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setRx(freq, active);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setRx(freq, active);
 }
 
 void afv_native::api::atcClient::SetXc(unsigned int freq, bool active) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setXc(freq, active);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setXc(freq, active);
 }
 
 void afv_native::api::atcClient::SetOnHeadset(unsigned int freq, bool active) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setOnHeadset(freq, active);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setOnHeadset(freq, active);
 }
 
 bool afv_native::api::atcClient::GetOnHeadset(unsigned int freq) {
-    return client->getOnHeadset(freq);
+  return client->getOnHeadset(freq);
 }
 
 bool afv_native::api::atcClient::GetTxActive(unsigned int freq) {
-    return client->getTxActive(freq);
+  return client->getTxActive(freq);
 };
 
 bool afv_native::api::atcClient::GetRxActive(unsigned int freq) {
-    return client->getRxActive(freq);
+  return client->getRxActive(freq);
 };
 
 bool afv_native::api::atcClient::GetTxState(unsigned int freq) {
-    return client->GetTxState(freq);
+  return client->GetTxState(freq);
 };
 
 bool afv_native::api::atcClient::GetXcState(unsigned int freq) {
-    return client->GetXcState(freq);
+  return client->GetXcState(freq);
 };
 
 bool afv_native::api::atcClient::GetRxState(unsigned int freq) {
-    return client->GetRxState(freq);
+  return client->GetRxState(freq);
 };
 
-void afv_native::api::atcClient::UseTransceiversFromStation(std::string station, int freq) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->linkTransceivers(station, freq);
+void afv_native::api::atcClient::UseTransceiversFromStation(std::string station,
+                                                            int freq) {
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->linkTransceivers(station, freq);
 };
 
-int afv_native::api::atcClient::GetTransceiverCountForStation(std::string station) {
-    auto tcs = client->getStationTransceivers();
-    return tcs[station].size();
+int afv_native::api::atcClient::GetTransceiverCountForStation(
+    std::string station) {
+  auto tcs = client->getStationTransceivers();
+  return tcs[station].size();
 };
 
 void afv_native::api::atcClient::SetRadiosGain(float gain) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setRadioGainAll(gain);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setRadioGainAll(gain);
 }
 
 void afv_native::api::atcClient::FetchTransceiverInfo(std::string station) {
-    client->requestStationTransceivers(station);
+  client->requestStationTransceivers(station);
 }
 
 void afv_native::api::atcClient::GetStation(std::string station) {
-    client->getStation(station);
+  client->getStation(station);
 }
 
 void afv_native::api::atcClient::FetchStationVccs(std::string station) {
-    client->requestStationVccs(station);
+  client->requestStationVccs(station);
 }
 
 void afv_native::api::atcClient::SetPtt(bool pttState) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setPtt(pttState);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setPtt(pttState);
 }
 
 std::string afv_native::api::atcClient::LastTransmitOnFreq(unsigned int freq) {
-    return client->lastTransmitOnFreq(freq);
+  return client->lastTransmitOnFreq(freq);
 }
 
-void afv_native::api::atcClient::AddFrequency(unsigned int freq, std::string stationName) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->addFrequency(freq, true, stationName);
+void afv_native::api::atcClient::AddFrequency(unsigned int freq,
+                                              std::string stationName) {
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->addFrequency(freq, true, stationName);
 }
 
 void afv_native::api::atcClient::RemoveFrequency(unsigned int freq) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->removeFrequency(freq);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->removeFrequency(freq);
 }
 
 bool afv_native::api::atcClient::IsFrequencyActive(unsigned int freq) {
-    return client->isFrequencyActive(freq);
+  return client->isFrequencyActive(freq);
 }
 
 void afv_native::api::atcClient::SetAtisRecording(bool state) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setRecordAtis(state);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setRecordAtis(state);
 }
 
 bool afv_native::api::atcClient::IsAtisRecording() {
-    return client->isAtisRecording();
+  return client->isAtisRecording();
 }
 
 void afv_native::api::atcClient::SetAtisListening(bool state) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->listenToAtis(state);
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->listenToAtis(state);
 }
 
 bool afv_native::api::atcClient::IsAtisListening() {
-    return client->isAtisListening();
+  return client->isAtisListening();
 }
 
-void afv_native::api::atcClient::StartAtisPlayback(std::string callsign, unsigned int freq) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->startAtisPlayback(callsign, freq);
+void afv_native::api::atcClient::StartAtisPlayback(std::string callsign,
+                                                   unsigned int freq) {
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->startAtisPlayback(callsign, freq);
 }
 
 void afv_native::api::atcClient::StopAtisPlayback() {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->stopAtisPlayback();
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->stopAtisPlayback();
 }
 
-void afv_native::api::atcClient::SetHardware(afv_native::HardwareType hardware) {
-    std::lock_guard<std::mutex> lock(afvMutex);
-    client->setHardware(hardware);
+void afv_native::api::atcClient::SetHardware(
+    afv_native::HardwareType hardware) {
+  std::lock_guard<std::mutex> lock(afvMutex);
+  client->setHardware(hardware);
 }
 
 bool afv_native::api::atcClient::IsAtisPlayingBack() {
-    return client->isAtisPlayingBack();
+  return client->isAtisPlayingBack();
 }
 
-void afv_native::api::atcClient::RaiseClientEvent(std::function<void(afv_native::ClientEventType, void*, void*)> callback)
-{
-    client->ClientEventCallback.addCallback(nullptr, [callback](afv_native::ClientEventType evt, void* data, void* data2)
-    {
-        callback(evt, data, data2);
-    });
+void afv_native::api::atcClient::RaiseClientEvent(
+    std::function<void(afv_native::ClientEventType, void *, void *)> callback) {
+  client->ClientEventCallback.addCallback(
+      nullptr, [callback](afv_native::ClientEventType evt, void *data,
+                          void *data2) { callback(evt, data, data2); });
 }
