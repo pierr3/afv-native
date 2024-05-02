e5350028f6da230d7fac4e50eca3686c571e52c5
Pierre Ferran <pierr3@users.noreply.github.com>
2024-05-02 15:40:48 -0400

    New release workflow, support for arm64 linux
    bloody hell

---
+++ Findafv-native.cmake
@@ -0,0 +1,22 @@
+# Findafv-native.cmake
+
+find_path(AFV_NATIVE_INCLUDE_DIR
+  NAMES afv_native.h
+  PATHS ${afv_native_SOURCE_DIR}/include
+)
+
+find_library(AFV_NATIVE_LIBRARY
+  NAMES afv_native libafv_native
+  PATHS ${afv_native_SOURCE_DIR}/lib
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(afv_native DEFAULT_MSG AFV_NATIVE_LIBRARY AFV_NATIVE_INCLUDE_DIR)
+
+if(AFV_NATIVE_FOUND AND NOT TARGET afv_native)
+  add_library(afv_native UNKNOWN IMPORTED)
+  set_target_properties(afv_native PROPERTIES
+    IMPORTED_LOCATION "${AFV_NATIVE_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${AFV_NATIVE_INCLUDE_DIR}"
+  )
+endif()
