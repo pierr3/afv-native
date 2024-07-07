c34d266656827a27f72dd8fdeaaa625c33a0193b
Pierre Ferran <pierr3@users.noreply.github.com>
2024-07-07 16:03:10 -0400

    Revert "Refactor audio device initialization"
    This reverts commit 8b6bdc61e87b646a99b7b3da4bb9e95a82587ca1.

--- include/afv-native/audio/AudioDevice.h
+++ include/afv-native/audio/AudioDevice.h
@@ -74,87 +74,15 @@ namespace afv_native { namespace audio {
          */
         typedef unsigned int Api;
 
-            /** Ensures data within the abstract is zeroed.   Should always be called via
-             * the initialiser chain of any subclasses.
-             */
-            AudioDevice();
-
-        public:
-            /** Abstract API ID type - it is up to the implementing driver to ensure that
-             * the mapping is uniform in any given session.  Persistent mapping of values
-             * is not guaranteed between sound backends or successive executions of the
-             * code.  A uniform method for determining API ID should be provided by the
-             * driver.
-             */
-            typedef unsigned int Api;
-
-            /** DeviceInfo is a uniform structure by which API implementations can return
-             * information about known devices.  The "id" value should be used as an
-             * argument to the constructor of the driver instance to set the desired device.
-             */
-            struct DeviceInfo {
-                std::string name;
-                std::string id;
-                explicit DeviceInfo(std::string name, std::string id = "");
-            };
-
-            virtual ~AudioDevice();
-
-            /** open() should open and start the playback and capture of the nominated audio
-             * streams that this device is bound to.  If the streams are already playing,
-             * then it should return success.
-             *
-             * @return true if open() was successful, false otherwise.
-             */
-            virtual bool openOutput() = 0;
-            virtual bool openInput() = 0;
-
-            /** close() should stop and shutdown the playback and capture of the nominated
-             * audio streams.  If the streams are already stopped, it must do nothing.
-             */
-            virtual void close() = 0;
-
-            /** setSource sets the ISampleSource for this AudioDevice.
-             *
-             * Any existing source will have it's pointer released.
-             *
-             * This can be set to the invalid/empty pointer to disable the source, in which
-             * case the device should output silence.
-             *
-             * @param newSrc a shared_ptr to the new source.
-             */
-            virtual void setSource(std::shared_ptr<ISampleSource> newSrc);
-
-            /** setSink sets the ISampleSink for this AudioDevice.
-             *
-             * Any existing sink will have its pointer released.
-             *
-             * This can be set to the invalid/empty pointer to disable the sink, in which
-             * case the device should simply discard any samples received from the hardware.
-             *
-             * @param newSink a shared_ptr to the new Sink
-             */
-            virtual void setSink(std::shared_ptr<ISampleSink> newSink);
-
-            /** OutputUnderflows is a monotonic counter of the number of playback buffer
-             * underflows that have occurred since the AudioDevice was constructed.
-             */
-            std::atomic<uint32_t>   OutputUnderflows;
-
-            /** InputOverflows is a monotonic counter of the number of recording buffer
-             * overflows that have occurred since the AudioDevice was constructed.
-             */
-            std::atomic<uint32_t>   InputOverflows;
-
-            /* default implementation hooks... */
-            static std::map<Api,std::string> getAPIs();
-            static std::map<int,DeviceInfo> getCompatibleInputDevicesForApi(AudioDevice::Api api);
-            static std::map<int,DeviceInfo> getCompatibleOutputDevicesForApi(AudioDevice::Api api);
-            static std::shared_ptr<AudioDevice> makeDevice(
-                    const std::string &userStreamName,
-                    const std::string &deviceName,
-                    Api audioApi = -1,
-                    bool splitChannels = false);
+        /** DeviceInfo is a uniform structure by which API implementations can return
+         * information about known devices.  The "id" value should be used as an
+         * argument to the constructor of the driver instance to set the desired device.
+         */
+        struct DeviceInfo {
+            std::string name;
+            std::string id;
+            bool        isDefault;
+            explicit DeviceInfo(std::string name, bool isDefault = false, std::string id = "");
         };
 
         virtual ~AudioDevice();
