576431154c6c8fdd93581293ea498996477a79e4
Mike Evans <mike@downstairsgeek.com>
2024-10-11 00:42:38 -0400

    poco exception

--- src/cryptodto/UDPChannel.cpp
+++ src/cryptodto/UDPChannel.cpp
@@ -41,11 +41,14 @@
 using namespace afv_native::cryptodto;
 using namespace std;
 
-UDPChannel::UDPChannel(int receiveSequenceHistorySize):
+UDPChannel::UDPChannel(int receiveSequenceHistorySize) try :
     Channel(), mAddress(), mDatagramRxBuffer(nullptr), mPocoUDPSocket(), mPocoSocketReactor(), mReactorThread("UDP Socket Reactor Thread"), mTxSequence(0), receiveSequence(0, receiveSequenceHistorySize), mAcceptableCiphers(1U << cryptodto::CryptoDtoMode::CryptoModeChaCha20Poly1305), mDtoHandlers(), mLastErrno(0) {
     LOG("udpchannel","Entry");
     mDatagramRxBuffer = new unsigned char[maxPermittedDatagramSize];
     mReactorThread.start(mPocoSocketReactor);
+}
+catch (Poco::Net::ServiceNotFoundException & e) {
+    LOG("updchannel","Service Not Found Exception: %s %s",e.displayText().c_str(),e.message().c_str());
 }
 
 UDPChannel::~UDPChannel() {
